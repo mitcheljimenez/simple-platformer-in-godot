f03de633b528febd888d8221fbbabf72ab2ef3e6
Mitchel <mitcheljimrod@gmail.com>
2025-01-31 05:37:14 -0300

    feat: add enemy scene and script; implement basic movement and collision detection

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
+    Engine.time_scale = 0.5
     timer.start()
+    body.get_node("CollisionShape2D").queue_free()
     pass
 
 func _on_timer_timeout() -> void:
+    Engine.time_scale = 1.0
     get_tree().reload_current_scene()
